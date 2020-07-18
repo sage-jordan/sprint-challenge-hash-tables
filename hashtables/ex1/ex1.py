e5cd7a43f593cc268e8f54c4bba19b66fc59d368
sage-jordan <sagemjordan@gmail.com>
2020-07-17 22:13:32 -0400

    ex1 passing!!

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,23 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
+def get_indices_of_item_weights(weights, length, limit):
+
+    cache = {}
+    # iterate over list
+    for i in range(length):
+        # print(i)
+        number = weights[i]
+        # calculate weight needed
+        weightNeeded = limit - number
+        # if this value isn't in our cache
+        if weightNeeded not in cache:
+        # add this num:i to cache
+            cache[number] = i
+            # print(cache)
+            continue
+        # if value needed is in cache, return these indexes
+        weightNeeded = cache[weightNeeded]
+        if i < weightNeeded:
+            return (weightNeeded, i)
+        else:
+            return (i, weightNeeded)
     return None
