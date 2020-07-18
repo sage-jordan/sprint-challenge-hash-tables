a9ec9cf7547c68732a9845a0d5d8e299335e4b8b
sage-jordan <sagemjordan@gmail.com>
2020-07-18 00:37:24 -0400

    ex2 works!! Realized I could refactor before looping

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,29 +6,17 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
+    # restructure tickets with dict comprehention
+    tickets = {x.source:x for x in tickets}
+
+    route = []
     
-    cache = {}
-    route = [None] * length
+    # set current 
+    current = tickets["NONE"]
 
-    for i in range(length):
-        source = tickets[i].source
-        destination = tickets[i].destination
-        # if this pair isn't stored
-        if source is not None:
-            if source not in cache:
-                # store it
-                cache[source] = destination
-        else:
-            route.append(source)
-            route.append(destination)
-            while destination in cache:
-                route.append(cache[destination])
-                destination = cache[destination]
-            if destination is not None:
-                if source not in cache:
-                    # store it
-                    cache[source] = destination
-            else:
-                route.append(source)
+    while current.destination != "NONE":
+        route.append(current.destination)
+        current = tickets[current.destination]
 
+    route.append(current.destination)
     return route
